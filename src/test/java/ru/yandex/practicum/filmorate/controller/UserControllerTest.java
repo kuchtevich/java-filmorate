bdb403a415c51381a3b52af076b489d53cd16dc1
User Daria <kuchtevich@yandex.tu>
2024-08-28 04:30:28 +0300

    ТЗ-12

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,44 +24,6 @@ class UserRepositoryTest {
     @Autowired
     UserStorage userStorage;
 
-    @Test
-    void shouldGetAllUsersTest() {
-        Optional<Collection<User>> userList = Optional.ofNullable(userStorage.allUsers());
-        assertThat(userList)
-                .isPresent()
-                .hasValueSatisfying(user -> {
-                    assertThat(user).isNotEmpty();
-                    assertThat(user).hasSize(5);
-                    assertThat(user).element(0).hasFieldOrPropertyWithValue("id", 1L);
-                    assertThat(user).element(1).hasFieldOrPropertyWithValue("id", 2L);
-                    assertThat(user).element(2).hasFieldOrPropertyWithValue("id", 3L);
-                    assertThat(user).element(3).hasFieldOrPropertyWithValue("id", 4L);
-                    assertThat(user).element(4).hasFieldOrPropertyWithValue("id", 5L);
-                });
-    }
-
-    @Test
-    void shouldCreateUserTest() {
-        User newUser = new User();
-        newUser.setLogin("Surname5");
-        newUser.setName("user5");
-        newUser.setEmail("mail5@yandex.com");
-        newUser.setBirthday(LocalDate.of(2020, 8, 19));
-
-        Optional<User> userOptional = Optional.ofNullable(userStorage.addUser(newUser));
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user -> {
-                            assertThat(user).hasFieldOrPropertyWithValue("id", 5L);
-                            assertThat(user).hasFieldOrPropertyWithValue("login", "Surname5");
-                            assertThat(user).hasFieldOrPropertyWithValue("name", "user5");
-                            assertThat(user).hasFieldOrPropertyWithValue("email",
-                                    "mail5@yandex.com");
-                            assertThat(user).hasFieldOrPropertyWithValue("birthday",
-                                    LocalDate.of(2020, 8, 19));
-                        }
-                );
-    }
 
     @Test
     void shouldUpdateUserTest() {
