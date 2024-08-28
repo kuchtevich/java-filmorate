056309319305c0d1d3df21d2575aed201b0cc8c3
User Daria <kuchtevich@yandex.tu>
2024-08-28 20:50:55 +0300

    ТЗ-12

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.MpaStorage;
